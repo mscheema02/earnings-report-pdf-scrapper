2fc4b03af357c84e17e998a058d387b4b5a8813d
MSC <manvircheema02@gmail.com>
2022-05-04 15:18:05 -0400

    Init update

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
-df = pd.read_csv("file_path / file_name.")
-i = 0;
-output = "last"
-while(i < 5):
-    print (i)
-    i=i+1
-print("... then the", output, "value is", str(i))
+# df = pd.read_csv("file_path / file_name.")
+# i = 0;
+# output = "last"
+# while(i < 5):
+#     print (i)
+#     i=i+1
+# print("... then the", output, "value is", str(i))
+
+import requests
+response = requests.get("http:/randomfox.ca/floof")
+print(response.status_code)
