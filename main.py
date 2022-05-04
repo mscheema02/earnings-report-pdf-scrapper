6ff67eb3719ec49cde2376bcebd3cf94cf452502
MSC <manvircheema02@gmail.com>
2022-05-04 13:04:22 -0400

    Init Update

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("file_path / file_name.")
+i = 0;
+output = "last"
+while(i < 5):
+    print (i)
+    i=i+1
+print("... then the", output, "value is", str(i))
