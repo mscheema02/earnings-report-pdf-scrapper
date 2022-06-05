b75096868660192d86a63550042cf4085fa60d38
MSC <manvircheema02@gmail.com>
2022-06-05 19:42:47 -0400

    Updates

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import pdfquery
 
 # df = pd.read_csv("file_path / file_name.")
 filePath = "./er-testers/20220427_alphabet_10Q.pdf"
-i = 0;
-
+df = tabula.read_pdf(filePath, pages = 'all')
 
 # pdf = pdfquery.PDFQuery('C:/Projs/earnings-report-pdf-scrapper/er-testers/20220427_alphabet_10Q.pdf')
 # pdf.load()
